145102aba930d7e620b2afbd08503b17d5eba8bb
Renzi Fidele <renzifidele2001@gmail.com>
2025-04-18 15:43:15 +0200

    feat: Migrando o template do Single e do Content

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,15 +1,8 @@
-<article @php(post_class())>
-  <header>
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        {!! $title !!}
-      </a>
-    </h2>
-
-    @include('partials.entry-meta')
-  </header>
-
-  <div class="entry-summary">
-    @php(the_excerpt())
+<div class="post--item col-md-4">
+  <div class="post--image" style="background-image: url('{{ the_post_thumbnail_url()}}');"></div>
+  <div class="post--text">
+    <div class="post--categories">{{ the_category(' • ') }}</div>
+    <a class="post--title" href="{{ the_permalink() }}">{{ the_title() }}</a>
   </div>
-</article>
+  <div class="post--excerpt">{{ the_excerpt(__('(more…)')) }}</div>
+</div>
--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,7 +1,35 @@
+@php
+$image_url = get_the_post_thumbnail_url($post, 'large');
+$images = get_field('gallery');
+$featuredText = get_field('featured_text');
+@endphp
+
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
-  @endwhile
+
+@while(have_posts()) @php(the_post())
+<section class="jumbo is--post" style="background-image: url({{ the_post_thumbnail_url() }})">
+</section>
+<article>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="title--page is--post">{{ the_title() }}</h1>
+        <div class="post--categories is--post">{{ the_category(' • ') }} </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="text--intro is--post">
+          {{ the_content() }}
+        </div>
+
+      </div>
+    </div>
+  </div>
+</article>
+@includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
+@endwhile
+
 @endsection
